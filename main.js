6286a3022cf1cd4fd9fb617c7295528e628f0c5d
moseskereya <lazaromoses77@gmail.com>
2019-06-30 14:40:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var countDownDate = new Date("Jun 24, 2020 00:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector(".timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
